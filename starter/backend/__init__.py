f3a21925914dd1618b82fe14526d3be43ca96567
“nthuuluan” <nthuuluan@gmai.com>
2024-05-06 15:08:53 +0700

    .

--- starter/backend/__init__.py
+++ starter/backend/__init__.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     app.run(
         debug=True,
         host="0.0.0.0",
-        port=int(os.getenv("FLASK_RUN_PORT", 5000)),
+        port=int(os.getenv("FLASK_RUN_PORT", "")),
     )
